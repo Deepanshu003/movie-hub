32656c5bde94fff40d6ec44b05fa58af5acfdc33
deepanshu <ldeepanshu4@gmail.com>
2022-11-02 00:22:55 -0700

    deepanshu added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './Home/Home';
+// import Movie from './Movie/Movie';
+import {BrowserRouter as Router, Routes, Route} from  "react-router-dom";
+import SingleMovie from './SingleMovie';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movie/:id' element={<SingleMovie />} />
+      </Routes>
+    </Router>
+   </>
+  )
 }
 
-export default App;
+export default App
+// const API_URL = `http://www.omdbapi.com/?s=kabhi&apikey=f97d2236`;
